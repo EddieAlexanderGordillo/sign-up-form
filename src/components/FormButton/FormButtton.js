470682ac309dcb2fbec0671d820275626b41ef8e
Felipe Andrés López <alexgordillo.g@gmail.com>
2022-05-21 19:28:40 -0500

    add Button Try

---
+++ src/components/FormButton/FormButtton.js
@@ -0,0 +1,7 @@
+const FormButton = () =>
+`<div>
+    <button type="submit" class="btnForm">CLAIM YOUR FREE TRIAL</button>
+</div>
+`;
+
+export default FormButton;
--- src/components/FormContainer/FormContainer.js
+++ src/components/FormContainer/FormContainer.js
@@ -1,6 +1,9 @@
+import FormButton from "../FormButton/FormButtton";
+
 const FormContainer = () =>
 `<div class="form-container">
   <form id="form" action=""></form>
+    ${FormButton()}
   <p class="disclaimer">
     By clicking the button, you are agreeing to our <a href="#">Terms and Services</a>
   </p>
